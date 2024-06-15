555dcc4a9a14539c08ddb3e2f4a06669b73e03f9
lirije <lirije11@hotmail.com>
2024-06-15 18:06:59 +0200

    Add images and update index.vue

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
 import { fetchProject } from "../composables/useProjectFetch";
 import { getImageUrl } from '../utils/helpers'; 
 import type { Project } from "../composables/useProjectFetch";
